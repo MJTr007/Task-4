1f52872969c72ee90398d3313a4b7fe28decea21
MJTr007 <manojram19944@gmail.com>
2022-12-07 22:22:49 +0530

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// var request = new XMLHttpRequest();
+// request.open("GET","https://restcountries.com/v3.1/all",true)
+// request.send()
+// request.onload=function()
+// {
+//     var data = request.response
+//     var result = JSON.parse(data)
+//      for(var obj of result)
+//      {
+//          console.log(obj.name.common , obj.region, obj.subregion ,
+//             obj.population);
+//      }
+// }
+// var request = new XMLHttpRequest();
+//  request.open("GET","https://restcountries.com/v3.1/all",true)
+// request.send()
+//  request.onload=function()
+// {
+//    var data = request.response
+//    var result = JSON.parse(data)
+//     for(var obj of result)
+//     {
+//       console.log(obj.name.common , obj.flags.png);
+//     }
+// }
+
+var a = {name:"Person 1", age:5};
+var b = {age:5, name:"Person 1"};
+
+if (JSON.stringify(a)===JSON.stringify(b));
+console.log(a,b)
